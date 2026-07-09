60f8411e0285fcd85b32759632fcb30a7eadbb4d
Yashtagad12 <tagadyash12@gmail.com>
2026-07-09 22:30:43 +0530

    changed path for some components after modifying them

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -6,12 +6,12 @@ export { default as Editor } from './Editor';
 export { default as Customers } from './Customers';
 export { default as ColorPicker } from './ColorPicker';
 export { default as Calender } from './Calender';
-export { default as Area } from './Charts/Area';
-export { default as Bar } from './Charts/Bar';
+export { default as AreaChartComponent } from "../Pages/charts/Area";
+export { default as BarChartComponent } from '../Pages/charts/Bar';
 export { default as ColorMapping } from './Charts/ColorMapping';
 export { default as Financial } from './Charts/Financial';
-export { default as Line } from './Charts/Line';
+export { default as LineChartComponent } from "../Components/Charts/linechart";
 export { default as Pie } from './Charts/Pie';
-export { default as Pyramid } from './Charts/Pyramid';
-export { default as Stacked } from './Charts/Stacked';
-
+export { default as Pyramid } from '../Pages/charts/Pyramid';
+export { default as Stacked } from '../Pages/charts/Stacked';
+export { default as Header } from "../Components/Header";
